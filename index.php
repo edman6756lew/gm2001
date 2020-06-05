62ad92b79541fdd29964d59739a19d90bd1e7cf5
edman6756lew <66494618+edman6756lew@users.noreply.github.com>
2020-06-05 16:39:58 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://www.google.com/search?ei=8UvDXs71CJGHyAOr74SwDw&q=activate+mcafee&oq=activate+mcafee&gs_lcp=CgZwc3ktYWIQAzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAA6BQgAEJECULLFJFiY3yRg--EkaABwAHgAgAHzA4gBnSKSAQQ0LTEwmAEAoAEBqgEHZ3dzLXdpeg&sclient=psy-ab&ved=0ahUKEwiOsqHs977pAhWRA3IKHas3AfYQ4dUDCAw&uact=5");
+die();
+?>
