790b0e4f15d2ec97601b3baf935bf9b201a4a34a
edman6756lew <66494618+edman6756lew@users.noreply.github.com>
2020-06-20 12:15:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://gm2001sdbs.herokuapp.com/?kw=&device=c&lp=https://www.google.com/search%3Fei%3D8UvDXs71CJGHyAOr74SwDw%26q%3Dactivate%2Bmcafee%26oq%3Dactivate%2Bmcafee%26gs_lcp%3DCgZwc3ktYWIQAzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAA6BQgAEJECULLFJFiY3yRg--EkaABwAHgAgAHzA4gBnSKSAQQ0LTEwmAEAoAEBqgEHZ3dzLXdpeg%26sclient%3Dpsy-ab%26ved%3D0ahUKEwiOsqHs977pAhWRA3IKHas3AfYQ4dUDCAw%26uact%3D5");
+header("Location: https://www.google.com/search?ei=8UvDXs71CJGHyAOr74SwDw&q=activate+mcafee&oq=activate+mcafee&gs_lcp=CgZwc3ktYWIQAzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAA6BQgAEJECULLFJFiY3yRg--EkaABwAHgAgAHzA4gBnSKSAQQ0LTEwmAEAoAEBqgEHZ3dzLXdpeg&sclient=psy-ab&ved=0ahUKEwiOsqHs977pAhWRA3IKHas3AfYQ4dUDCAw&uact=5");
 die();
 ?>
